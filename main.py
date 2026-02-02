ae1b07e3a1726d8e8d7f120571e460ecfdb963cd
Dewei Zhu <Dewei.zhu@stud.tu-darmstadt.de>
2026-02-02 11:50:45 +0100

    scale anpassen

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def measureTextWidth(text=""):
     return context.measureText(text).width
 
 def calculateClassWidth(umlClass):
-    scale = 1
+    scale = 0.75
     padding = 80 * scale
     minWidth = 365 * scale
     umlDiagram = document.getElementById("umlDiagram")
